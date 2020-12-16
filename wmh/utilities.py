35bafab53094743d1d3b49f08cd460a3e1e1290a
Ross Callaghan <ross.callaghan.16@ucl.ac.uk>
2020-12-16 10:10:17 +0000

    define pre and post processing functions

--- wmh/utilities.py
+++ wmh/utilities.py
@@ -1,1 +1,9 @@
 #!/usr/bin/env python
+import os
+import numpy as np
+import tensorflow as tf
+import difflib
+import SimpleITK as sitk
+
+def preprocessing(FLAIR_image, T1_image):
+def postprocessing(FLAIR_array, pred, rowcol_minmax):
