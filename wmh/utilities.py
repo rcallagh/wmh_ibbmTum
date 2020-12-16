7d3fe8d8b0f6a0ac0c344e8bc47835c7ff201e33
Ross Callaghan <ross.callaghan.16@ucl.ac.uk>
2020-12-16 10:18:47 +0000

    find out whether we need to shrink rows/cols

--- wmh/utilities.py
+++ wmh/utilities.py
@@ -7,4 +7,12 @@ import SimpleITK as sitk
 
 def preprocessing(FLAIR_image, T1_image, rowcol_info):
     #  start_slice = 10
+    channel_num = 2
+    print(np.shape(FLAIR_image))
+    num_selected_slice = np.shape(FLAIR_image)[0]
+    image_rows_Dataset = np.shape(FLAIR_image)[1]
+    image_cols_Dataset = np.shape(FLAIR_image)[2]
+
+    rows_to_shrink = image_rows_Dataset >= rowcol_info["rows_standard"]
+    cols_to_shrink = image_cols_Dataset >= rowcol_info["cols_standard"]
 def postprocessing(FLAIR_array, pred, rowcol_info):
