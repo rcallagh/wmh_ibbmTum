49eaed90cee09b0686669aad35dd28d3315546f0
Ross Callaghan <ross.callaghan.16@ucl.ac.uk>
2020-12-22 18:36:28 +0000

    typo

--- wmh/model.py
+++ wmh/model.py
@@ -119,7 +119,7 @@ def get_unet(img_shape = None, f_weight=None, lr=2e-4):
     conv10 = Conv2D(1, 1, 1, activation='sigmoid', dim_ordering=dim_ordering)(conv9)
     model = Model(input=inputs, output=conv10)
 
-    model.compile(optimizer=Adam(lr=lr, loss=dice_coef_loss, metrics=[dice_coef_for_training])
+    model.compile(optimizer=Adam(lr=lr), loss=dice_coef_loss, metrics=[dice_coef_for_training])
 
     if f_weight is not None:
         print("Previous checkpoint only contains weights. Loading in previous weights from {}, but initial training may be poor due to lack of trained optimizer.".format(f_weight))
