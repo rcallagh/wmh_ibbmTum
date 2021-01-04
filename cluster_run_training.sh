45990ad2c7806ad199b09d62e540425e4cc5ee22
Ross Callaghan <ross.callaghan.16@ucl.ac.uk>
2021-01-04 15:19:00 +0000

    script to run training on the cluster

---
+++ cluster_run_training.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+#====================
+# QSUB
+#====================
+#$ -l h_rt=12:0:0
+#$ -l tmem=12G
+#$ -l gpu=true
+#$ -N whm_train
+#$ -wd /SAN/medic/camino_2point0/Ross/wmh/
+#$ -S /bin/bash
+#$ -j y
+#$ -t 1
+
+#===== PYTHON/CUDA setup ============================
+# source /share/apps/
+source /share/apps/source_files/cuda/cuda-10.0.source
+#===================================================
+
+#Setup directories for WMH seg code, python venv
+WMH_FOLDER="/SAN/medic/camino_2point0/Ross/wmh/"
+PY_VENV_FOLDER="/home/rcallagh/py_venv/wmh/"
+
+#Location of training and testing data
+train_hdf="/SAN/medic/camino_2point0/Ross/wmh/biobank_train_data.hdf5"
+test_hdf="/SAN/medic/camino_2point0/Ross/wmh/biobank_test_data.hdf5"
+
+#Network params
+num_unet=1
+num_unet_start=${SGE_TASK_ID}
+
+# Create folder for python venv if does not exist already
+if [ ! -d ${PY_VENV_FOLDER} ]; then
+mkdir $PY_VENV_FOLDER
+fi
+# Create python virtual environment if does not exist
+if [ -f ${PY_VENV_FOLDER}/bin/activate ]; then
+        echo "Sourcing ${PY_VENV_FOLDER}/bin/activate ..."
+        source ${PY_VENV_FOLDER}/bin/activate
+else
+        echo "Creating virtual environment in ${PY_VENV_FOLDER}"
+        python3 -m venv ${PY_VENV_FOLDER}
+        source ${PY_VENV_FOLDER}/bin/activate
+        pip install -r ${FAST_SURFER_FOLDER}/requirements.txt
+fi
+
+
+date
+hostname
+# Change directory to the main FS
+cd ${WMH_FOLDER}
+# Define the three planes
+# Define the outputs
+input_train="${out_dir_train}/training_set_${pln}.hdf5"
+input_valid="${out_dir_valid}/validation_set_${pln}.hdf5"
+# Assumes to restart the training from the provided checkpoints
+# First letter of the plane is capital
+cap_pln="$(tr '[:lower:]' '[:upper:]' <<< ${pln:0:1})${pln:1}"
+log_dir="../checkpoints/${cap_pln}_Weights_FastSurferCNN"
+# Run the python function for training set
+python train.py \
+       --hdf5_name_train ${train_hdf} \
+       --batch_size 32 \
+       --epochs 60 \
+       --resume \
+       --num_unet ${num_unet} \
+       --num_unet_start ${num_unet_start}
+
+
+date
