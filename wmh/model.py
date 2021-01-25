09c0cdb130ca21f9af433eda2251ec19a5def7d7
Ross Callaghan <ross.callaghan.16@ucl.ac.uk>
2021-01-25 15:59:21 +0000

    move losses to new file

--- wmh/model.py
+++ wmh/model.py
@@ -13,19 +13,8 @@ from evaluation import getDSC, getHausdorff, getLesionDetection, getAVD, getImag
 from keras import backend as K
 import h5py
 
-### ----define loss function for U-net ------------
-smooth = 1
-def dice_coef_for_training(y_true, y_pred):
-    print(np.shape(y_pred))
-    y_true_f = K.flatten(y_true)
-    y_pred_f = K.flatten(y_pred)
-    intersection = K.sum(K.abs(y_true_f * y_pred_f))
-    return (2. * intersection + smooth) / (K.sum(K.square(y_true_f)) + K.sum(K.square(y_pred_f)) + smooth)
+from wmh.losses import * #Get all the loss functions
 
-def dice_coef_loss(y_true, y_pred):
-    print(np.shape(y_pred))
-    print(np.shape(y_true))
-    return -dice_coef_for_training(y_true, y_pred)
 
 def get_crop_shape(target, refer):
         # width, the 3rd dimension
