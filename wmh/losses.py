d25064ae39d96636651d735df1f5b405167a7bba
Ross Callaghan <ross.callaghan.16@ucl.ac.uk>
2021-01-26 13:27:29 +0000

    update dice loss
    to 1 - DSC

--- wmh/losses.py
+++ wmh/losses.py
@@ -7,16 +7,16 @@ from keras import backend as K
 ### ----define loss function for U-net ------------
 smooth = 1
 def dice_coef_for_training(y_true, y_pred):
-    print(np.shape(y_pred))
+    # print(np.shape(y_pred))
     y_true_f = K.flatten(y_true)
     y_pred_f = K.flatten(y_pred)
     intersection = K.sum(K.abs(y_true_f * y_pred_f))
     return (2. * intersection + smooth) / (K.sum(K.square(y_true_f)) + K.sum(K.square(y_pred_f)) + smooth)
 
 def dice_coef_loss(y_true, y_pred):
-    print(np.shape(y_pred))
-    print(np.shape(y_true))
-    return -dice_coef_for_training(y_true, y_pred)
+    # print(np.shape(y_pred))
+    # print(np.shape(y_true))
+    return 1-dice_coef_for_training(y_true, y_pred)
 
 def jaccard_distance_loss(y_true, y_pred, smooth=100):
     """
