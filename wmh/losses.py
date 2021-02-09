c71c6b11c5d32cd6ad04422acca67576547fc993
Ross Callaghan <ross.callaghan.16@ucl.ac.uk>
2021-02-09 09:18:54 +0000

    typo

--- wmh/losses.py
+++ wmh/losses.py
@@ -60,7 +60,7 @@ def focal_tversky(y_true,y_pred):
 all_losses = {
     'dice_coef_for_training': dice_coef_for_training,
     'dice_coef_loss': dice_coef_loss,
-    'jaccard_distace_loss': jaccard_distance_loss,
+    'jaccard_distance_loss': jaccard_distance_loss,
     'tversky': tversky,
     'tversky_loss': tversky_loss,
     'focal_tversky': focal_tversky
